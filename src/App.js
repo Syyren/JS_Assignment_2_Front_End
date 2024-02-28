1c8f8074017087880d7e6e0d07dc44fa03597ee4
Syyren <129496054+Syyren@users.noreply.github.com>
2024-02-27 17:41:13 -0700

    moved files to proper locations, set routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import About from '../pages/About'
-import ContactDelete from '../pages/ContactDelete'
-import ContactDetails from '../pages/ContactDetails'
-import ContactEdit from '../pages/ContactEdit'
-import Contacts from '../pages/Contacts'
-import Home from '../pages/Home'
-import NoPage from '../pages/NoPage'
+import About from './pages/About'
+import ContactDelete from './pages/ContactDelete'
+import ContactDetails from './pages/ContactDetails'
+import ContactEdit from './pages/ContactEdit'
+import Contacts from './pages/Contacts'
+import Home from './pages/Home'
+import NoPage from './pages/NoPage'
 
 export default function App() {
   return 
@@ -14,7 +14,13 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={ <Home /> } />
-          
+          <Route path="/home" element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/contacts" element={ <Contacts /> } />
+          <Route path="/contact/details" element={ <ContactDetails /> } />
+          <Route path="/contact/delete" element={ <ContactDelete /> } />
+          <Route path="/contact/edit" element={ <ContactEdit /> } />
+          <Route path="*" element={ <NoPage /> } />
         </Routes>
       </BrowserRouter>
     </div>
