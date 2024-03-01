8660b15751f6a00032483657c1e97d456599dc86
Syyren <129496054+Syyren@users.noreply.github.com>
2024-02-29 17:18:42 -0700

    setting up delete

--- src/pages/ContactDelete.js
+++ src/pages/ContactDelete.js
@@ -1,7 +1,11 @@
+import { useParams, Link } from 'react-router-dom';
 import Layout from "../components/Layout";
+import { getContact } from '../controllers/ContactController';
 
 export default function ContactDelete()
 {
+    const { contactID, slug } = useParams();
+
     return(
         <Layout>
             <h2 className="display-4 mb-4">Contact Delete Page</h2>
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,17 +1,35 @@
+//setting my endpoint URL
 const API_URL = 'http://localhost:8000'; //setting the api url
 
+//function that returns all contacts in the database
 export const fetchContacts = async () => 
 {
   const response = await fetch(`${API_URL}/contacts/view/all`);
   return response.json();
 };
 
+//function that fetches a contact via its id from the database
 export const fetchContact = async (contactID) => 
 {
   const response = await fetch(`${API_URL}/contacts/view/${contactID}`);
   return response.json();
 };
 
+//function that deletes a contact from the database
+export const deleteContact = async (contactID) => 
+{
+  const response = await fetch(`${API_URL}/contacts/del/${contactID}`, {
+    method: 'DELETE'
+  });
+  //checking if the deletion went through properly
+  if (!response.ok) 
+  {
+    throw new Error('Failed to delete contact');
+  }
+  console.log('Contact deleted successfully.');
+};
+
+//function that returns all categories in the database
 export const fetchCategories = async () => 
 {
   const response = await fetch(`${API_URL}/categories/view/all`);
